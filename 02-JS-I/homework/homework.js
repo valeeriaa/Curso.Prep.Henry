4f6f6dbf9c9f16f5c8cd92bf3a08efb9ba34920e
Valeeriaa <valeriasiepe@hotmail.com>
2021-09-11 11:10:55 -0300

    35 ejercicios resueltos de JSI

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -221,14 +221,12 @@ function deEuroAdolar(euro){
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
   return euro * 1.2
-  
 }
-
-
-function esVocal(letra){
-  //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
-  //Verificar si el usuario ingresó un string de más de un carácter y, en ese caso, informarle 
-  //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
+ function esVocal(letra){
+  //Escribe una función que reciba una letray, si es una vocal, muestre el mensaje "Es vocal"
+  //Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle
+  //que no se puede procesar el dato mediante el mensaje "Dato incorrecto"
+  //Si no es vocal, también debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   if (letra.Lenght > 1) {
     return "Dato incorrecto"
